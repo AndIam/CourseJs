ceca2f890fe3af9b8d1b1fa067fc1e95721321db
Andrey <exammpplle@rmail.org>
2023-10-05 21:31:47 +0300

    First exercise

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-alert('123');
+const numberOffilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const prsonalMovieDB =
+{
+    count: numberOffilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const firstQuestion = prompt('Один из последних просмотренных фильмов?', '');
+const secondQuestion = prompt('На сколько оцените его?', '');
+
+prsonalMovieDB.movies[firstQuestion] = secondQuestion;
+
+console.log(prsonalMovieDB.movies);
+
